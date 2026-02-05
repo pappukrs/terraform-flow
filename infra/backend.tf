fe3d327ab224ccf4d6bf6230ae0362216781d32f
GitHub Action <action@github.com>
2026-02-05 13:36:02 +0000

    chore: auto-update terraform backend bucket to pappukrs611-2026-terraform-state

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,7 +2,7 @@
 # Format: <your-suffix>-terraform-state
 terraform {
   backend "s3" {
-    bucket         = "CHANGE_ME_TO_YOUR_BUCKET_NAME" # <--- MUST UPDATE THIS MANUALLY
+    bucket         = "pappukrs611-2026-terraform-state" # <--- MUST UPDATE THIS MANUALLY
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
