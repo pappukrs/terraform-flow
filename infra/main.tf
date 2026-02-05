64d169730586047e4f681c1948eb7241dfb692dd
pappukrs <digitalpappu101@gmail.com>
2026-02-05 22:29:39 +0530

    fix: add force_delete to ecr for destroy

--- infra/main.tf
+++ infra/main.tf
@@ -283,6 +283,7 @@ resource "aws_eks_node_group" "eks_nodes" {
 resource "aws_ecr_repository" "app_repo" {
   name                 = var.ecr_repository_name
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
