9e99780d80b65ddd470966bf46b8857c3b2e0d40
pappukrs <digitalpappu101@gmail.com>
2026-02-05 19:14:02 +0530

    fix: bump eks version to 1.31 and legacy ecr conflict

--- infra/main.tf
+++ infra/main.tf
@@ -192,10 +192,10 @@ resource "aws_security_group" "eks_cluster_sg" {
 }
 
 # EKS Cluster
-resource "aws_eks_cluster" "eks_cluster" {
+  resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
-  version  = "1.28"
+  version  = "1.31"
 
   vpc_config {
     subnet_ids              = [
